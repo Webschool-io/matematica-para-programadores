1fbae98879ec3acf94936084dfe80a3b8e74b705
Suissa <jnascimento@gmail.com>
2016-09-16 07:56:47 -0300

    ajeitandooo

--- primos/testes/validators/isCoPrimo.js
+++ primos/testes/validators/isCoPrimo.js
@@ -1,20 +1,15 @@
 const getAllDivisors = require('../helpers/getAllDivisors.js')
-
-const flatten = (a, b) => {
-  return a.concat(b);
-}
-const count = (names) => 
+const flatten = (a, b) => a.concat(b)
+const mapeiaDivisores = (names) => 
   names.reduce((a, b) => 
     Object.assign(a, {[b]: (a[b] || 0) + 1}), {})
 
-const duplicates = (dict) => 
+const achaDuplicados = (dict) => 
   Object.keys(dict).filter((a) => dict[a] > 1)
 
-
 const test = (arr) => {
-
   const flat = arr.map(getAllDivisors).reduce(flatten, [])
-  return (duplicates(count(flat)).length === 1) 
+  return (achaDuplicados(mapeiaDivisores(flat)).length === 1) 
 }
 
 module.exports = test
