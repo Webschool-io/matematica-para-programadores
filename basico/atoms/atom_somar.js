1777af172cd3e0d46f47d9e044682747b8f88b1d
Daniel <danielsalles@gmail.com>
2016-12-14 16:45:22 -0200

    Melhorando palavra.

--- basico/atoms/atom_somar.js
+++ basico/atoms/atom_somar.js
@@ -1,4 +1,4 @@
-//Agora aceita multi parametros de forma mais simples
+//Agora lida com multi parametros de forma mais simples
 //Funcional <3
 const reduce = (array) => array.reduce((total, element) => total += element, 0)
 const soma = (...args) => reduce(args);
