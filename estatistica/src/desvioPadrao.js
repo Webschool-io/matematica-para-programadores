8a79a10b0fb441f3fd337aeac3597ccd0455cecd
Lukas Wilkeer <lukasc4l@gmail.com>
2017-02-01 17:37:00 -0200

    Modificando o código das medidas de dispersões para ficar mais didático.

--- estatistica/src/desvioPadrao.js
+++ estatistica/src/desvioPadrao.js
@@ -1,22 +1,8 @@
 const arr = [2,5,3,2]
-const produtoNotavel = require('./produtoNotavel')
-const media = require('./media')
-// console.log('media', media)
-const raizQuadrada = (valor) => Math.sqrt(valor)
+const variancia = require('./variancia')
 
-const desvioPadrao = (amostra) => {
-  const m = media.calcular(amostra)
-  const somatorio = (acc, cur) => {
-    let produto = produtoNotavel.menos(acc, m)
-    console.log('produto', produto)
-    acc += produto
-    return acc
-  }
-  const raizDentro = amostra.reduce(somatorio)
-  const raizDividida = raizDentro / amostra.length
-  return raizQuadrada(raizDividida)
-}
+const desvio = (arr) => Math.sqrt(variancia(arr))
 
-console.log(desvioPadrao(arr))
+console.log('desvio padrão: ', desvio(arr))
 
-// module.exports = (arr) => arr.reduce(media)
+module.exports = (arr) => desvio(arr)
---
+++ estatistica/src/variancia.js
@@ -0,0 +1,24 @@
+#! /usr/bin/node
+const m = require('./media.js')
+
+//const somatorio = (arr) => arr.reduce((a, b) => a + b)
+const somatorio = (a, b) => a + b
+
+const diferenca = (arr, media, result = []) => {
+  if (arr.length === 0) { 
+    return result
+  } else {
+    result.push(arr.pop() - media)
+    return diferenca(arr, media, result)
+  }
+}
+
+const variancia = (lista) => {
+  let tamanho = lista.length
+  let media = m.calcular(lista)
+  let diferencaMedia = diferenca(lista, media)
+  let result = diferencaMedia.reduce(somatorio)/tamanho
+  return result
+}
+
+module.exports = variancia
