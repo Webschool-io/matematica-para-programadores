5314c921d1940dcfc67d6afc8df2309527ffddf0
Suissa <jnascimento@gmail.com>
2017-02-07 04:59:06 -0200

    Adicionando unidades para o conversor universal

--- unidades-medida/index.js
+++ unidades-medida/index.js
@@ -11,3 +11,17 @@ const converter = (val, base, to) => unities[base][to](val)
 // console.log('converter 0c => f', converter(0, 'c', 'f'))
 // console.log('converter 20c => f', converter(20, 'c', 'f'))
 // console.log('converter 100c => f', converter(100, 'c', 'f'))
+
+
+// console.log('converter 1cal => J', converter(1, 'cal', 'J'))
+// console.log('converter 10cal => J', converter(10, 'cal', 'J'))
+
+
+// console.log('converter 1torr => atm', converter(1, 'torr', 'atm'))
+// console.log('converter 1atm => torr', converter(1, 'atm', 'torr'))
+
+
+console.log('converter 1km/h => m/min', converter(1, 'km/h', 'm/min'))
+console.log('converter 1km/h => m/s', converter(1, 'km/h', 'm/s'))
+console.log('converter 1km/h => cm/min', converter(1, 'km/h', 'cm/min'))
+console.log('converter 1km/h => cm/s', converter(1, 'km/h', 'cm/s'))
--- unidades-medida/unities.js
+++ unidades-medida/unities.js
@@ -1,14 +1,36 @@
 module.exports =  { 
   'c': {
+    name: 'Celsius',
     'f': (val) => (val * 1.8  + 32),
     'k': (val) => (val + 273.15)
   },
   'f': {
+    name: 'Fahreneit',
     'c': (val) => (val  - 32) / 1.8,
     'k': (val) => (val * 1.8) - 459.67
   },
-  'k': {
+  'K': {
+    name: 'Kelvin',
     'c': (val) => (val  - 273.15),
     'f': (val) => (val * 1.8) - 459.67
+  },
+  'cal': {
+    name: 'Caloria',
+    'J': (val) => (val  * 4.184),
+  },
+  'torr': {
+    name: 'Torr',
+    'atm': (val) => (val  * 0.001315785918),
+  },
+  'atm': {
+    name: 'ATM',
+    'torr': (val) => (val  / 0.001315785918),
+  },
+  'km/h': {
+    name: 'Kilômetro por hora',
+    'm/min': (val) => (val / (60/1000) ),
+    'm/s': (val) => (val  / (3600/1000)),
+    'cm/min': (val) => (val  / (60/100000) ),
+    'cm/s': (val) => (val  / (3600/100000) ),
   }
 }
