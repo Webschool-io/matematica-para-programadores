f0e02bd3c3b4f524d280ea9928cb5aedbdea9dc1
Lukas Wilkeer <lukasc4l@gmail.com>
2017-02-09 19:20:37 -0200

    Fix: fixDecimal retornando string e bugando os cálculos.

--- estatistica/src/media.js
+++ estatistica/src/media.js
@@ -1,9 +1,7 @@
 const somatorio = require('./somatorio.js')
-const numeros = require('../../ferramentas/numeros.js')
-const cincoCasas = numeros.fixDecimal(5)
 
 // media = Soma de itens/numero de itens
 const numeroDeItens = (arr) => arr.length
-const media = (arr) => cincoCasas(arr.reduce(somatorio)/ numeroDeItens(arr))
+const media = (arr) => arr.reduce(somatorio)/ numeroDeItens(arr)
 
 module.exports = media
