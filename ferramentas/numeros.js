6f1e27b549f5db2158f2656a2bb9eeacb7290487
Lukas Wilkeer <lukasc4l@gmail.com>
2017-02-10 15:51:44 -0200

    Função para precisão dos numeros e testes.

--- ferramentas/numeros.js
+++ ferramentas/numeros.js
@@ -1,4 +1,9 @@
 const fixDecimal = (casas) => (numero) => Number(numero).toFixed(casas)
 const isNumber = (x) => Number(x) === x
 
-module.exports = ({fixDecimal, isNumber})
+const precisao = (numero, p) => {
+  let fator = Math.pow(10, p)
+  return (Math.round(numero * fator)/fator)
+}
+
+module.exports = ({fixDecimal, isNumber, precisao})
--- tests/ferramentas/numeros.test.js
+++ tests/ferramentas/numeros.test.js
@@ -1,14 +1,36 @@
 const chai = require('chai')
 const expect = chai.expect
 const should = chai.shoud
-const numeros = require('../../ferramentas/numeros.js')
+const number = require('../../ferramentas/numeros.js')
+const cincoCasas = number.fixDecimal(5)
 
 describe('fixDecimal', () => {
-  it('should throw error if not Number')
-
-  it('should add decimals', (done) => {
-    let cincoCasas = numeros.fixDecimal(5)
-    expect(cincoCasas(1)).to.be === 1.0000
+  it('should add decimals for print porpouse', (done) => {
+    expect(cincoCasas(1)).to.be === '1.0000'
     done()
+  })
+})
+
+describe('precision', () => {
+  it('should return a Number', (done) => {
+    let n = number.precisao(1, 5)
+    expect(n).to.be.a('number')
+    done()
+  })
+
+  it('should set precision to 5', (done) => {
+    let n = number.precisao(1, 5)
+    expect(n).to.be.eql(1.0000)
+    done()
+  })
+
+  describe('isNumber', () => {
+    it('should verify if is a number', (done) => {
+      let n = Number(5)
+      let s = 'string'
+      expect(number.isNumber(n)).to.be.true
+      expect(number.isNumber(s)).to.be.false
+      done()
+    })
   })
 })
